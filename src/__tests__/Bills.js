5ffdc050609ba2763da6a0b9b33f8ad0d089190d
nabil-y <nabil.yassine.dev@gmail.com>
2022-04-16 19:28:00 +0200

    Increase login coverage and add test to bills

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import BillsUI from "../views/BillsUI.js";
 import { bills } from "../fixtures/bills.js";
 import { ROUTES_PATH } from "../constants/routes.js";
@@ -42,6 +42,53 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => (a < b ? 1 : -1);
       const datesSorted = [...dates].sort(antiChrono);
       expect(dates).toEqual(datesSorted);
+    });
+    // new test for new bill form button
+    test("Then if I click on new bill button, the new bill form should be displayed", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getByTestId("btn-new-bill"));
+      const newBillButton = screen.getByTestId("btn-new-bill");
+      fireEvent.click(newBillButton);
+      await waitFor(() => screen.getByTestId("form-new-bill"));
+      const newBillForm = screen.getByTestId("form-new-bill");
+      expect(newBillForm).toBeTruthy();
+    });
+
+    // new test for modal view of bill
+    test("Then if I click the eye icon of a bill, the bill modal should be displayed", async () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      await waitFor(() => screen.getAllByTestId("icon-eye"));
+      const billEyeIcons = screen.getAllByTestId("icon-eye");
+      console.debug(billEyeIcons);
+      // fireEvent.click(billEyeIcons[0]);
+      // const modal = screen.getByRole("dialog");
+      // expect(modal.style.display).toBe("block");
     });
   });
 });
