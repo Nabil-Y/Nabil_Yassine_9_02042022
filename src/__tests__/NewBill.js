e292a2d3921952ecc63dc27ebed436837b9c5c56
nabil-y <nabil.yassine.dev@gmail.com>
2022-04-20 13:43:33 +0200

    Progress on New Bill tests

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { fireEvent, screen } from "@testing-library/dom";
+import { fireEvent, screen, waitFor } from "@testing-library/dom";
 import NewBillUI from "../views/NewBillUI.js";
 import NewBill from "../containers/NewBill.js";
 import { localStorageMock } from "../__mocks__/localStorage.js";
@@ -41,6 +41,40 @@ describe("Given I am on NewBill Page", () => {
       submitButton.addEventListener("click", handleSubmitMock);
       fireEvent.click(submitButton);
       expect(handleSubmitMock).toHaveBeenCalled();
+    });
+  });
+  describe("When I submit a file", () => {
+    test("Then handleSubmit function should be called", () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      const newBillMock = new NewBill({
+        document,
+        onNavigate,
+        store: null,
+        localStorage: localStorageMock,
+      });
+
+      //to-do write assertion
+      const fileButton = screen.getByTestId("file");
+      const handleChangeMock = jest.fn(() => newBillMock.handleChangeFile);
+      fileButton.addEventListener("change", handleChangeMock);
+      fireEvent.change(fileButton);
+      expect(handleChangeMock).toHaveBeenCalled();
     });
   });
 });
